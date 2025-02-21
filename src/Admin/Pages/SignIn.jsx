debce622567e5b8793f95f79234da8a49462e5fa
LeMinhHieu2004 <leminhhieu27062004@gmail.com>
2025-02-21 20:13:19 +0700

    signin

--- src/Admin/Pages/SignIn.jsx
+++ src/Admin/Pages/SignIn.jsx
@@ -2,6 +2,28 @@ import React from 'react'
 
 const SignIn = () => {
   return (
+    <>
+  <meta charSet="utf-8" />
+  <title>Sign In | Velzon - Admin &amp; Dashboard Template</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta
+    content="Premium Multipurpose Admin & Dashboard Template"
+    name="description"
+  />
+  <meta content="Themesbrand" name="author" />
+  {/* App favicon */}
+  <link rel="shortcut icon" href="assets/images/favicon.ico" />
+  {/* Layout config Js */}
+  {/* Bootstrap Css */}
+  <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+  {/* Icons Css */}
+  <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+  {/* App Css*/}
+  <link href="assets/css/app.min.css" rel="stylesheet" type="text/css" />
+  {/* custom Css*/}
+  <link href="assets/css/custom.min.css" rel="stylesheet" type="text/css" />
+
+
     <div>
       <div className="auth-page-content">
   <div className="container">
@@ -142,6 +164,7 @@ const SignIn = () => {
 </div>
 
     </div>
+    </>
   )
 }
 
